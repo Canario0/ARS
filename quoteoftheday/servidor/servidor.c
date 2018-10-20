27b06d3170d5680e6443f2b043bc0f1228cbd90b
Pablo <prenero4bc@gmail.com>
2018-10-20 22:59:42 +0200

    Servidor comentado

--- quoteoftheday/servidor/servidor.c
+++ quoteoftheday/servidor/servidor.c
@@ -15,7 +15,6 @@
 void output(int const pos, char const *argv[], const int total);
 void paramError();
 void ayuda();
-void ipError(const char *in);
 void portError();
 void setPort(const int port);
 
@@ -25,11 +24,13 @@ uint16_t local_port;
 int main(int argc, char const *argv[])
 {
     // Bloque datos de entrada
+    //Compruebo que si hay parametros de entrada, no pueden ser más de dos
     if ((argc - 1) > 2)
     {
         paramError();
     }
 
+    //Llamo a la función para procesar los datos o en el caso de no tener parámetros establezco el puerto por defecto de qotd
     if ((argc - 1) <= 2 && (argc - 1) > 0)
     {
         output(1, argv, argc - 1);
@@ -48,13 +49,15 @@ int main(int argc, char const *argv[])
 
     //Bloque del socket
     int id_sock;
-    id_sock = socket(AF_INET, SOCK_DGRAM, 0);
 
+    //Creamos el socket y comprobamos los posibles errores
+    id_sock = socket(AF_INET, SOCK_DGRAM, 0);
     if (id_sock == -1)
     {
         perror("socket()");
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
+    //Imprimo el id del socket para dar un feedback al usuario
     printf("Id de socket %d\n\n", id_sock);
     //Fin bloque del socket
 
@@ -64,29 +67,32 @@ int main(int argc, char const *argv[])
     local_addr.sin_family = AF_INET;
     local_addr.sin_port = local_port;
     local_addr.sin_addr.s_addr = INADDR_ANY;
-
+    //Hacemos el bind con el puerto y comprobamos los errores
     error = bind(id_sock, (struct sockaddr *)&local_addr, sizeof(local_addr));
     if (error < 0)
     {
         perror("bind()");
+        //En el caso de que el bind falle el socket se queda encendido, por eso hay que cerrarlo
         error = close(id_sock);
         if (error < 0)
         {
             perror("close()");
-            exit(-1);
+            exit(EXIT_FAILURE);
         }
-        exit(-1);
+        exit(EXIT_FAILURE);
     }
     //Fin bloque de bind
 
     //Bloque de escucha
-
+    //Buffers de entrada y salida
     char data_out[512] = "";
     char data_in[512] = "";
     static char buffQuote[MAXLENGTH] = "";
     struct sockaddr_in remote_addr;
+    socklen_t len = sizeof(remote_addr);
     while (1)
     {
+        //Primera parte de la cadena de salida
         if (!strcpy(data_out, "Quote Of The Day from vm2511:\n"))
         {
             perror("strcpy()");
@@ -94,18 +100,18 @@ int main(int argc, char const *argv[])
             if (error < 0)
             {
                 perror("close()");
-                exit(-1);
+                exit(EXIT_FAILURE);
             }
             exit(EXIT_FAILURE);
         }
-
+        //llamamos al sistema para ejecutar el comando fortune
         if (system("/usr/games/fortune -s > /tmp/tt.txt") == -1)
         {
             perror("system()");
             exit(EXIT_FAILURE);
         }
 
-        socklen_t len = sizeof(remote_addr);
+        //Esperamos que llegue un mensaje del cliente y comprobamos posibles errores
         error = recvfrom(id_sock, &data_in, 512, 0, (struct sockaddr *)&remote_addr, &len);
         if (error < 0)
         {
@@ -114,14 +120,15 @@ int main(int argc, char const *argv[])
             if (error < 0)
             {
                 perror("close()");
-                exit(-1);
+                exit(EXIT_FAILURE);
             }
-            exit(-1);
+            exit(EXIT_FAILURE);
         }
-
+        //Mostramos la ip del cliente y los datos recibidos
         printf("Mensaje de la ip: %s\n", inet_ntoa(remote_addr.sin_addr));
         printf("\033[1;32mContenido:\033[0m %s\n", data_in);
 
+        //leemos el quote hasta llenar 482 caracteres o el fin de fichero
         FILE *fich = fopen("/tmp/tt.txt", "r");
         if (!fich)
         {
@@ -130,7 +137,7 @@ int main(int argc, char const *argv[])
             if (error < 0)
             {
                 perror("close()");
-                exit(-1);
+                exit(EXIT_FAILURE);
             }
             exit(EXIT_FAILURE);
         }
@@ -146,14 +153,14 @@ int main(int argc, char const *argv[])
                 if (error < 0)
                 {
                     perror("close()");
-                    exit(-1);
+                    exit(EXIT_FAILURE);
                 }
                 exit(EXIT_FAILURE);
             }
             buffQuote[nc++] = aux;
             aux = fgetc(fich);
         } while (nc < MAXLENGTH - 1 && aux != EOF);
-
+        //cerramos el fichero
         if (fclose(fich) == EOF)
         {
             perror("fclose()");
@@ -161,10 +168,11 @@ int main(int argc, char const *argv[])
             if (error < 0)
             {
                 perror("close()");
-                exit(-1);
+                exit(EXIT_FAILURE);
             }
             exit(EXIT_FAILURE);
         }
+        //concatenamos las cadenas
         if (!strcat(data_out, buffQuote))
         {
             perror("strcpy()");
@@ -172,11 +180,12 @@ int main(int argc, char const *argv[])
             if (error < 0)
             {
                 perror("close()");
-                exit(-1);
+                exit(EXIT_FAILURE);
             }
             exit(EXIT_FAILURE);
         }
 
+        //mandamos el quote
         error = sendto(id_sock, data_out, sizeof(data_out), 0, (struct sockaddr *)&remote_addr, sizeof(remote_addr));
         if (error < 0)
         {
@@ -185,20 +194,36 @@ int main(int argc, char const *argv[])
             if (error < 0)
             {
                 perror("close()");
-                exit(-1);
+                exit(EXIT_FAILURE);
             }
-            exit(-1);
+            exit(EXIT_FAILURE);
         }
     }
 
+    //close
+    //cierro el socket
+    error = close(id_sock);
+    if (error < 0)
+    {
+        perror("close()");
+        exit(EXIT_FAILURE);
+    }
     //Fin bloque de escucha
     return 0;
 }
 
+/**
+ * Función que parsea los argumentos de entrada
+ * pos posición a parsear
+ * argv argumentos de entrada
+ * total total de argumentos
+ */
 void output(int const pos, char const *argv[], const int total)
 {
+    //opción para mostrar la ayuda
     if (strcmp(argv[pos], "-h") == 0)
     {
+        //En el caso de que la opción -h no esté en la pos 1 o tenga más argumentos detras se lanza un error de entrada
         if (pos != 1 || total != 1)
         {
             paramError();
@@ -207,7 +232,7 @@ void output(int const pos, char const *argv[], const int total)
     }
     else if (strcmp(argv[pos], "-p") == 0)
     {
-        // printf("Esto es el puerto\n");
+        //En el caso de que el puerto no vaya seguido de un elemento o el elemento no sea un número se lanza un error
         int port;
         if (total != 2)
             paramError();
@@ -217,35 +242,40 @@ void output(int const pos, char const *argv[], const int total)
         }
         else
         {
+            //Función que establece el puerto
             setPort(port);
         }
     }
 }
+
+/**
+ * Función que imprime un mensaje de error en los parámetros
+ */
 void paramError()
 {
     printf("Número de parámetros erroneo\nPruebe './%s -h' para más información.\n", PROGRAM_NAME);
-    exit(-1);
+    exit(EXIT_FAILURE);
 }
-
+/**
+ * Función que imprime la ayuda desplegada con la opción -h
+ */
 void ayuda()
 {
     printf("\nUso: ./%s [-p puerto-servidor]\n\n", PROGRAM_NAME);
     printf("Opciones:\n\t-p\n\t   Si no se proporciona un número de puesto se usará el puerto por defecto del servicio Quote of the Day.\n\n");
     exit(0);
 }
-
-void ipError(const char *in)
-{
-    printf("\nLa ip: '%s' no es válidad\n\n", in);
-    exit(-1);
-}
-
+/**
+ * Función que imprime un mensaje de error si el puerto no es válido
+ */
 void portError()
 {
     printf("\nEl puerto no es válido o no se ha podido encontrar\n\n");
-    exit(-1);
+    exit(EXIT_FAILURE);
 }
-
+/**
+ * Función que traduce un puerto de número al valor de internet
+ */
 void setPort(const int port)
 {
     local_port = htons(port);
