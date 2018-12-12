f443a77bb7cd558bda4d75af161c60071b908d98
Pablo <prenero4bc@gmail.com>
2018-12-12 12:51:45 +0100

    Pendiente de probar

--- ICMP/cliente/cliente.c
+++ ICMP/cliente/cliente.c
@@ -102,7 +102,7 @@ int main(int argc, char const *argv[])
 		printf(FLECHA_VERDE"Tamaño de la respuesta: %d\n", error);
 		printf(FLECHA_VERDE"Cadena recibida: %s\n", echo_response.payload);
 		printf(FLECHA_VERDE"Identifier (pid): %d\n",echo_response.ID);
-		printf(FLECHA_VERDE"TTL: %s\n",(char *)echo_response.ipHeader.TTL);
+		printf(FLECHA_VERDE"TTL: %u\n",echo_response.ipHeader.TTL);
 	}
 	printf("Type: %d Code: %d\n", echo_response.icmpHeader.Type, echo_response.icmpHeader.Code);
 
